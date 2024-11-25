f588887abc21bd3e496e31f0079beb7e998ccf39
adrienm7 <moyaux.adrien@gmail.com>
2024-11-25 18:41:48 +0100

    Version presque fonctionnelle en statique et sans bdd

--- files/static/php/matomo.php
+++
@@ -1,2 +0,0 @@
-<script src="/static/js/code-matomo.js" async></script>
-        <noscript><p class="d-none"><img src="https://stats.beseven.fr/matomo.php?siteid=2&amp;rec=1" style="border:0;" alt="" /></p></noscript>
